1476fa88063c5c2bc2b55a597ee1d97198803e23
fabriciojr26 <147607636+fabriciojr26@users.noreply.github.com>
2025-05-22 18:48:44 -0300

    Update index.js

--- index.js
+++ index.js
@@ -73,6 +73,8 @@ app.post("/webhook", async (req, res) => {
   }
 });
 
-app.listen(3000, () => {
-  console.log("Webhook CAPI ativo na porta 300");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Webhook CAPI ativo na porta ${PORT}`);
 });
+
